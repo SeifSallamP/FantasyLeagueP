51d15ca4c59276a262342c20df5ad5e8999fe047
omar6018 <64559722+omar6018@users.noreply.github.com>
2021-01-09 22:48:17 +0200

    Add files via upload

--- FantasyLeague/src/FileLinesMethods.java
+++ FantasyLeague/src/FileLinesMethods.java
@@ -1,9 +1,20 @@
+
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Scanner;
 import java.util.regex.Pattern;
 
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author dell
+ */
 public class FileLinesMethods {
     //Compares given name(Player's name) with player name in fileSystem/User FileSystem
     boolean lineNameSkipper_Comparer(String obj, File file) throws FileNotFoundException{
